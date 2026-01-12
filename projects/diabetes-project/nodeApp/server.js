196c47e579c7d56619941427cb5a84f0cac64bd4
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2026-01-12 12:09:17 +0200

    chore : folder organisation

---
+++ projects/diabetes-project/nodeApp/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const axios = require("axios");
+const bodyparser = require("body-parser");
+
+const app = express();
+app.use(bodyparser.json());
+app.use(express.static("public"));
+
+app.post("/prediction", async (req, res) => {
+  try {
+    const response = await axios.post("http://localhost:5000/predict", {
+      features: req.body.features,
+    });
+    res.json(response.data);
+    console.log(response.data.prediction);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("app running on http://localhost:3000");
+});
