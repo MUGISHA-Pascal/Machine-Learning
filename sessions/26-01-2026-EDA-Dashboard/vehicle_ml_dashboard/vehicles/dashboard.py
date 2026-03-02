64a9b48a1a18a59e5989b8dc2a70476842d7f9c1
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2026-03-02 12:11:59 +0200

    feat : session table functions

--- sessions/26-01-2026-EDA-Dashboard/vehicle_ml_dashboard/vehicles/dashboard.py
+++ sessions/26-01-2026-EDA-Dashboard/vehicle_ml_dashboard/vehicles/dashboard.py
@@ -20,9 +20,36 @@ def frequency_table(df):
     return table_html
 def sales_visualization_table(df):
     df['profit']=df['selling_price']-df['wholesale_price']
-    df.groupby(['manufacturer','transmission','fuel_type']).agg({
+    table_html= df.groupby(['manufacturer','transmission','fuel_type']).agg({
         "profit":"sum",
         "selling_price":['sum','count'],
-        "wholesale_price":['sum','count']
-    })
+        "wholesale_price":'sum'
+    }).to_html(
+        classes="table table-bordered table-striped table-sm",
+        float_format='%.2f',
+        justify='center'
+    )
+    return table_html
+def cross_tabulation_visualization_table(df):
+    table_html=pd.crosstab(df['manufacturer'],df['transmission'],margins=True).to_html(
+        classes="table table-bordered table-striped table-sm",
+        float_format='%.2f',
+        justify='center'
+    )
+    return table_html
+def multi_dimensional_tabulation_visualization_table(df):
+    table_html=pd.crosstab([df['manufacturer'],df['body_type']],[df['engine_type'],df['transmission']],margins=True).to_html(
+    classes="table table-bordered table-striped table-sm",
+    float_format='%.2f',
+    justify='center'
+    )
+    return table_html
+def cross_tabulation_with_more_details_visualization_table(df):
+    table_html=pd.crosstab(df['manufacturer'],df['body_type'],values=df['selling_price'],aggfunc='sum',margins=True).to_html(
+        classes="table table-bordered table-striped table-sm",
+        float_format='%.2f',
+        justify='center'
+    )
+    return table_html
+
 
